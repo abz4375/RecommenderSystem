f36a353cdf0e093228a2b93c28f08acab0da4c0e
Ayush Srivastava <abz4375.ayushsrivastava@gmail.com>
2024-12-11 20:29:59 +0530

    prod-fixes

--- app.py
+++ app.py
@@ -247,4 +247,4 @@ def get_recommendations():
 if __name__ == '__main__':
     logger.info('🚀 Starting Hotel Recommender Server...')
     # app.run(debug=True, port=5000)
-    app.run(debug=True, port=8501)
+    app.run(host='0.0.0.0', port=8080)
